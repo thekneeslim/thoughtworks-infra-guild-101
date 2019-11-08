b8c820bca4a1fb807d32929da959e9f4a1205cd4
Denise <denise.lim.sh@gmail.com>
2019-11-08 17:49:14 +0800

    Terraform configurations to spin up EC2 instance

---
+++ week_2/example.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "example" {
+  ami             = "ami-2757f631"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.ingress-all-test.id]
+  key_name        = var.key_name
+  subnet_id       = aws_subnet.subnet-uno.id
+  user_data       = <<-EOF
+    #!/usr/bin/env bash
+    echo "Installing java ..."
+    sudo add-apt-repository ppa:openjdk-r/ppa
+    sudo apt-get update
+    sudo apt-get -y install openjdk-8-jdk
+    echo "Downloading application ..."
+    curl -O -L "https://github.com/Thoughtworks-SEA-Capability/Infrastructure-101-Pathway/raw/master/week1/hello-spring-boot-0.1.0.jar"
+    echo "Starting application ..."
+    java -jar hello-spring-boot-0.1.0.jar
+    EOF
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
+
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
+
+//network.tf
+resource "aws_vpc" "test-env" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+//subnets.tf
+resource "aws_subnet" "subnet-uno" {
+  cidr_block        = aws_vpc.test-env.cidr_block
+  vpc_id            = aws_vpc.test-env.id
+  availability_zone = "us-east-1a"
+}
+
+//subnets.tf
+resource "aws_route_table" "route-table-test-env" {
+  vpc_id = aws_vpc.test-env.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test-env-gw.id
+  }
+}
+
+resource "aws_route_table_association" "subnet-association" {
+  subnet_id      = aws_subnet.subnet-uno.id
+  route_table_id = aws_route_table.route-table-test-env.id
+}
+
+//gateways.tf
+resource "aws_internet_gateway" "test-env-gw" {
+  vpc_id = aws_vpc.test-env.id
+}
+
+//security.tf
+resource "aws_security_group" "ingress-all-test" {
+  name   = "allow-all-sg"
+  vpc_id = aws_vpc.test-env.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 8080
+    to_port   = 9000
+    protocol  = "tcp"
+  }
+  // Terraform removes the default rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ week_2/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  default = "deployer-key"
+}
+
+variable "public_key" {}
